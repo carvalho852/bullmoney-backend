456e8229e072ef1db53a248d733f669ebd84677b
VINICIUS CARVALHO <viniciuspessoal852@gmail.com>
2025-06-26 11:22:46 -0300

    Corrigido host/porta para Render

--- api.py
+++ api.py
@@ -35,6 +35,9 @@ def stop():
 def status():
     return jsonify(bot.status())
 
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
